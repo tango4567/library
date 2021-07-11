9d3e64e3357d9b59def752369a8d9b36db9c0fb1
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2021-07-11 10:05:01 +0530

    Enable Full Screen

---
+++ utils/src/main/java/com/sowmitras/utils/EnableFullScreen.java
@@ -0,0 +1,42 @@
+package com.sowmitras.utils;
+
+import android.app.Activity;
+import android.view.View;
+/**
+ * For More Details Visit: https://developer.android.com/training/system-ui/immersive
+ **/
+public class EnableFullScreen {
+
+    private Activity activity;
+
+    public EnableFullScreen(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    public void showSystemUI() {
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+}
